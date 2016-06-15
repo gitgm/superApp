e51d6894da4918466d7de6797f74318bc48dd8ae
gitgm <mora.guillaum3@gmail.com>
2016-06-15 14:28:17 +0200

    starter

---
+++ www/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('accueilCtrl', function($scope) {
+
+})
+   
+.controller('catGorieCtrl', function($scope) {
+
+})
+   
+.controller('conversationsCtrl', function($scope) {
+
+})
+      
+.controller('pageThemeCtrl', function($scope) {
+
+})
+ 
