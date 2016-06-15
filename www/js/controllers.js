c6aee8c3a31e2076c7a8df2a2fd9c2a6a60ddc8c
gitgm <mora.guillaum3@gmail.com>
2016-06-15 14:34:33 +0200

    controller.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,4 +15,28 @@ angular.module('app.controllers', [])
 .controller('pageThemeCtrl', function($scope) {
 
 })
- 
+
+.controller('AppCtrl', function($scope, TwittSrv){
+  'use strict';
+  TwittSrv.getTwitts().then(function(twitts){
+    $scope.twitts = twitts;
+  });
+
+  $scope.doRefresh = function(){
+    TwittSrv.getNewTwitts().then(function(newTwitts){
+      $scope.twitts = newTwitts.concat($scope.twitts);
+    }).finally(function() {
+      // Stop the ion-refresher from spinning
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+  };
+
+  $scope.loadMore = function(){
+    TwittSrv.getMoreTwitts().then(function(olderTwitts){
+      $scope.twitts = $scope.twitts.concat(olderTwitts);
+    }).finally(function() {
+      // Stop the ion-infinite-scroll from spinning
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    });
+  };
+});
