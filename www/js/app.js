ea5354998c8afe474046129d4ff87c14e4eb9799
gitgm <mora.guillaum3@gmail.com>
2016-06-15 14:31:09 +0200

    app.js

--- www/js/app.js
+++ www/js/app.js
@@ -7,6 +7,31 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives'])
 
+.controller('AppCtrl', function($scope, TwittSrv){
+  'use strict';
+  TwittSrv.getTwitts().then(function(twitts){
+    $scope.twitts = twitts;
+  });
+
+  $scope.doRefresh = function(){
+    TwittSrv.getNewTwitts().then(function(newTwitts){
+      $scope.twitts = newTwitts.concat($scope.twitts);
+    }).finally(function() {
+      // Stop the ion-refresher from spinning
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+  };
+
+  $scope.loadMore = function(){
+    TwittSrv.getMoreTwitts().then(function(olderTwitts){
+      $scope.twitts = $scope.twitts.concat(olderTwitts);
+    }).finally(function() {
+      // Stop the ion-infinite-scroll from spinning
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    });
+  };
+})
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
