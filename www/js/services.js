44793f0b519746e08d9f45f0f9748e180d32dec1
gitgm <mora.guillaum3@gmail.com>
2016-06-15 23:45:14 +0200

    twitter maj

--- www/js/services.js
+++ www/js/services.js
@@ -8,18 +8,32 @@ angular.module('app.services', [])
 
 }])
 
-.factory('TwittSrv', function($q, $timeout){
+.factory('TwittSrv', function( $q, $timeout ){
   'use strict';
+
+	var twitterKey = 'STORAGE.TWITTER.KEY';
+	var clientId = '0HqOGPzoIhMMX4C9c6wMgFKf2';
+	var clientSecret = 'oAntqaCC2CarUSjglfqccOuW4g3YpXFIEOdBWeFMWWGdivTmq7';
+
+	function storeUserToken(data) {
+		window.localStorage.setItem(twitterKey, JSON.stringify(data));
+	}
+
+	function getStoredToken() {
+		return window.localStorage.getItem(twitterKey);
+	}
+
+
   var twitts = [
-    {user: {id: 'IonicFramework', name: 'ionic', avatar: 'https://pbs.twimg.com/profile_images/459365829348302849/lJ1X8rU9_bigger.png'}, content: 'We interviewed @gnomeontherun, author of the upcoming book @IonicinAction. Check it out: http://ionicframework.com/blog/ionic-in-action-book/', url: 'https://twitter.com/Ionicframework/status/569962126556708864'},
-    {user: {id: 'IonicBE', name: 'Ionic Belgium', avatar: 'https://pbs.twimg.com/profile_images/554941644579954688/xxp8o3Cq_bigger.png'}, content: 'Venez prendre un verre et discuter de votre project @Ionicframework au Monk bar @ Bxl ce vendredi à 20h!', url: 'https://twitter.com/IonicBE/status/569787372650385408'},
-    {user: {id: 'forum_hc', name: 'Human Coders Forum', avatar: 'https://pbs.twimg.com/profile_images/539837685481762816/cOO1F28S_bigger.png'}, content: 'Avez-vous testé Ionic Framework? https://forum.humancoders.com/t/avez-vous-teste-ionic-framework/1341/2', url: 'https://twitter.com/forum_hc/status/568806047051165696'},
-    {user: {id: 'loicknuchel', name: 'loic knuchel', avatar: 'https://pbs.twimg.com/profile_images/3133057797/81ea4e63c7078eec0a7c7d6ae57a3ce1_bigger.jpeg'}, content: 'Slides de mon #BBL sur @Ionicframework : http://loic.knuchel.org/blog/2015/02/19/talk-introduction-a-ionic-bbl-19-02-2015/ #hybrid #mobile #app', url: 'https://twitter.com/loicknuchel/status/568449174926176256'},
-    {user: {id: 'raymondcamden', name: 'Raymond Camden', avatar: 'https://pbs.twimg.com/profile_images/378800000568876933/2da22327d055cbf8e0502c3f22888fef_bigger.jpeg'}, content: 'Good list of @Ionicframework resources: https://github.com/Alexintosh/Awesome-Ionic', url: 'https://twitter.com/raymondcamden/status/568424487693082625'},
-    {user: {id: 'nraboy', name: 'Nic Raboy', avatar: 'https://pbs.twimg.com/profile_images/2653730816/5da4d8fb72352c715bbaffe07e56270e_bigger.jpeg'}, content: 'Use the native device calendar in your Android and iOS @IonicFramework mobile app using #ngCordova. https://blog.nraboy.com/2015/02/using-native-device-calendar-ionic-framework/ #appdev RT', url: 'https://twitter.com/nraboy/status/568438200198258689'},
-    {user: {id: 'devgirlFL', name: 'Holly Schinsky', avatar: 'https://pbs.twimg.com/profile_images/378800000664886768/5aa49c1cded0317a887cae28f5d80cd7_bigger.jpeg'}, content: 'What to expect in @Ionicframework 1.0: https://medium.com/@saniyusuf/looking-forward-to-ionic-v1-0-ionic-io-tools-6cb8e76e29c3 via @saniyusuf', url: 'https://twitter.com/devgirlFL/status/563673910424928256'},
-    {user: {id: 'maxlynch', name: 'Max Lynch', avatar: 'https://pbs.twimg.com/profile_images/546942133496995840/k7JAxvgq_bigger.jpeg'}, content: 'Awesome new Ionic course from @GoThinkster "Mastering Ionic - Learn to Build & Deploy Native Speed HTML5 Based Apps” https://thinkster.io/ionic-framework-tutorial/', url: 'https://twitter.com/maxlynch/status/568097163131006976'},
-    {user: {id: 'asdvaughan', name: 'Andrew Vaughan', avatar: 'https://pbs.twimg.com/profile_images/519532575031713792/Fm4zj2Zm_bigger.jpeg'}, content: 'Ionic Framework Demo - Matt Stauffer: https://www.youtube.com/watch?v=nh9EARpk-dc via @YouTube Great framework setup demo and api consumption!', url: 'https://twitter.com/asdvaughan/status/566103487281635328'}
+    {user: {id: 'IIMParis', name: 'IIM', avatar: 'https://pbs.twimg.com/profile_images/3225820577/caa93a85f082a27c8aab9a1bc7c784b3_400x400.png'}, content: 'La @LivingJoconde était de nouveau à @futur_en_seine cette année !', url: 'https://twitter.com/IIMparis'},
+    {user: {id: 'hetic', name: 'HETIC', avatar: 'https://pbs.twimg.com/profile_images/684039694733209602/AZtB1fkC_400x400.png'}, content: 'Réactions des Héticiens face aux sujets du #BacPhilo 2015 https://www.instagram.com/p/4CTEaBCH38/   #BAC2016 #Philo2016 #philo #tb', url: 'https://twitter.com/hetic'},
+    {user: {id: 'ESILVparis', name: 'ESILV', avatar: 'https://pbs.twimg.com/profile_images/2928019109/6f31a6034ed9c3e56723ec2bb5982808_400x400.jpeg'}, content: 'Zoom sur un projet de 5ème année, réalisé avec l @EMLVparis et en partenariat avec @HEINEKENFrance', url: 'https://twitter.com/ESILVparis/status/741184607396192256'},
+    {user: {id: 'EMLVParis', name: 'EMLV', avatar: 'https://pbs.twimg.com/profile_images/616226384701206528/RidfBtCB_400x400.jpg'}, content: 'Theres a hot new trend that could change the face of investing #FinTech #BigData', url: 'https://twitter.com/EMLVparis'},
+	{user: {id: 'HECParis', name: 'HEC', avatar: 'https://pbs.twimg.com/profile_images/666885423310966784/IepFD4tn_400x400.png'}, content: 'Traditional Learning vs. #elearning: "It all depends on what the participant is trying to achieve" #emba http://www.topmba.com/why-mba/faculty-voices/debate-traditional-learning-vs-distance-learning … @TopMBA', url: 'https://twitter.com/HECParis'},
+	{user: {id: 'Dauphine', name: 'Dauphines', avatar: 'https://pbs.twimg.com/profile_images/676802105101930497/WMK1KwwD_400x400.jpg'}, content: 'La société est-elle en avance sur ses élites concernant l #economiecollaborative? par Henri Isaac de @Paris_Dauphine', url: 'https://twitter.com/Paris_Dauphine'},
+	{user: {id: 'Fablab', name: 'Fablab Devinci', avatar: 'https://pbs.twimg.com/profile_images/529286149043281921/L7dI_dCV_400x400.jpeg'}, content: '6 solutions ont été primées ! http://msfr.so/WLPSnh  cc @esilvparis @iimparis @devincifablab @joriquier', url: 'https://twitter.com/DEVINCIFabLab'},
+	{user: {id: 'Shell eco', name: 'Shell Eco-marathon', avatar: 'https://pbs.twimg.com/profile_images/479364697544593410/SyDUGjk2_400x400.jpeg'}, content: 'In just 2 weeks you ll be on site for #SEM2016 at #makethefuture London. How s everyone shaping up? #LondonCalling', url: 'https://twitter.com/shell_ecomar'},
+	{user: {id: '42', name: '42', avatar: 'https://pbs.twimg.com/profile_images/719463883904315392/gNpb0Xqn_400x400.jpg'}, content: 'Become a superstar developer at @42born2codeUS and @42born2code  !', url: 'https://twitter.com/42born2code'},
   ];
 
   var service = {
